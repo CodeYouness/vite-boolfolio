c9e129f0b981cd945be6009c45496fec1dbbd810
CodeYouness <ylijassi@gmail.com>
2024-08-22 16:33:08 +0200

    added projectcard for single project and projectlist to view all project

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    props: {
+        title: {
+            typeof: String,
+            required: true
+        },
+        author: {
+            typeof: String,
+            required: true
+        },
+        image: {
+            typeof: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="project-container">
+        <div class="project-card">
+            <div class="project-image">
+                <img src="project-image-url.jpg" alt="Project Image">
+            </div>
+            <div class="project-details">
+                <h2 class="project-title">Project Title</h2>
+                <h3 class="project-author">Author: John Doe</h3>
+                <p class="project-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at ligula turpis.
+                </p>
+                <div class="project-category">
+                    <span>Category: Web Development</span>
+                </div>
+                <div class="project-technologies">
+                    <h4>Technologies Used:</h4>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,45 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+    components: {
+        ProjectCard
+    },
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    methods:{
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results.data);
+                this.projects = response.data.results.data;
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+        }
+    },
+    created(){
+        this.getProjects();
+    }
+}
+</script>
+
+<template>
+    <ul>
+        <ProjectCard v-for="project in projects">
+            {{ project.title }}
+        </ProjectCard>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
