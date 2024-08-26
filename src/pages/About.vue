dc112f07be13a47296d0cb24ea6d21c1a5682484
CodeYouness <ylijassi@gmail.com>
2024-08-26 16:47:59 +0200

    added vue router and folder of page about, blog and home

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<template>
+<h1>About</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Blog.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from 'axios';
+import ProjectList from './ProjectList.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        ProjectList
+    }
+}
+</script>
+
+<template>
+    <h1>Blog</h1>
+</template>
+
+<style scoped>
+
+</style>
